8afaaa9d53861f78b03edfdf7bfec97a656baaea
eveh@uw.edu <eveh@uw.edu>
2023-01-25 16:10:27 -0800

    added plot script

---
+++ scripts/plot_Eve.R
@@ -0,0 +1,11 @@
+library(here)
+library(tidyverse)
+file_path <- here::here("data", "pokemon.csv")
+poke_dat <- read.csv(file_path, header = T)
+
+#this is a comment
+
+#ggplot
+
+
+ggplot(poke_dat) + geom_point(mapping = aes(english_name, percent_male))
